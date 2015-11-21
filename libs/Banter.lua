25d12ec89fffe7c2bf05eef9dc39bc5eeecb9e80
Myke Atkinson <ploe@hotmail.co.uk>
2015-11-21 16:20:57 +0000

    Speed setting for the Banter worked in.

--- libs/Banter.lua
+++ libs/Banter.lua
@@ -3,11 +3,25 @@ Banter = {}
 font = love.graphics.newFont('res/font/banksia.ttf', 50) 
 love.graphics.setFont(font)
 
+-- set the speed of the Banter
+local speed = "FAST"
+function Banter.setSpeed(s)
+	speed = s
+end
+
+local settings = {
+	FAST = 1,
+	NORMAL = 0.2,
+	SLOW = 0.1, 
+}
+
+-- draw dat Banter 
 local msg = "hello, world"
 local last = 0 
 function Banter.draw()
-	last = last + 1
+	last = last + settings[speed]
 	local len = msg:len()
 	if last > len then last = len end
-	love.graphics.print(msg:sub(0, last), 0, 410)
+	love.graphics.print(msg:sub(0, math.floor(last)), 0, 410)
 end
+
